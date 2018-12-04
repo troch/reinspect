c32e4ce0a2086b84a52e91cd22bd77e3e33f125a
Thomas Roch <thomas.c.roch@gmail.com>
2018-12-04 23:43:00 +0000

    Update example

--- example/src/Counter.js
+++ example/src/Counter.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { useState } from "./reinspect"
+import { useState } from "reinspect"
 
 export function Counter({ id }) {
     const [count, setCount] = useState(
--- example/src/index.js
+++ example/src/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import { StateInspector, useState } from "./reinspect"
+import { StateInspector, useState } from "reinspect"
 import { Counter } from "./Counter";
 
 function App() {
